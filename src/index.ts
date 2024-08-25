e802a2efeb1071105837cc859e3c748678891cb1
Victor Oliveira <ovitor38@gmail.com>
2024-08-25 20:45:35 -0300

    feat(server): add server config and folders structure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config()
 
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
-});
+app.use(express.json()); // Para analisar corpos de requisições em JSON
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+export default app;
